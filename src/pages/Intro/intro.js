8d33711ea4b327d12a2a95b9f439c6b01d6d587d
rabiakhan01 <rabiakham789@gmail.com>
2024-03-21 11:50:37 +0500

    intro section is started now

---
+++ src/pages/Intro/intro.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Intro = () => {
+    return(
+        <div>
+        <h1 className="text-white">Intro</h1>
+        </div>
+    )
+}
+export default Intro;
---
+++ src/utils/layout/layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from '../../components/navbar/navbar';
+import Footer from "../../components/footer/footer";
+const Layout = ({children}) => {
+    return (
+        <div className="mr-10 ml-10 xl:mr-32 xl:ml-32 2xl:max-w-[1500px] 2xl:my-0 2xl:mx-auto">
+        <Navbar />
+        {children}
+        <Footer />
+        </div>
+    );
+}
+
+export default Layout;
