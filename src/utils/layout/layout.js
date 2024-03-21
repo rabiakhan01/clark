c0dcc6d05b686237a74691bc797efe50c6d2488e
rabiakhan01 <rabiakham789@gmail.com>
2024-03-21 11:26:54 +0500

    styles folder is created for custome styling

--- src/utils/layout/layout.js
+++ src/utils/layout/layout.js
@@ -3,7 +3,7 @@ import Navbar from '../../components/navbar/navbar';
 import Footer from "../../components/footer/footer";
 const Layout = ({children}) => {
     return (
-        <div className="mr-10 ml-10 xl:mr-24 xl:ml-24 2xl:max-w-[1500px] 2xl:my-0 2xl:mx-auto">
+        <div className="mr-10 ml-10 xl:mr-32 xl:ml-32 2xl:max-w-[1500px] 2xl:my-0 2xl:mx-auto">
         <Navbar />
         {children}
         <Footer />
