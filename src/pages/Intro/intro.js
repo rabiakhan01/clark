7531a6f5b5b19564f0f57d3545a35bea43a4a66e
rabiakhan01 <rabiakham789@gmail.com>
2024-03-21 12:59:24 +0500

    intro structure is completed

--- src/pages/Intro/intro.js
+++ src/pages/Intro/intro.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import images from "../../assets/images/images";
 const Intro = () => {
     return(
+        <div className="flex gap-20 mt-28 mb-28">
+        <div className="text-white flex flex-col w-2/5">
+        <span>hello!</span>
         <div>
-        <h1 className="text-white">Intro</h1>
+        <span>I'm </span>
+        <span>Clark Thompson</span>
+        </div>
+        <span>A Freelance Web Designer</span>
+        </div>
+        <div className="">
+        <img src= {images.background_1} alt="" />
+        </div>
         </div>
     )
 }
--- src/utils/layout/layout.js
+++ src/utils/layout/layout.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from '../../components/navbar/navbar';
-import Footer from "../../components/footer/footer";
+import Navbar from "../navbar/navbar";
+import Footer from "../footer/footer";
 const Layout = ({children}) => {
     return (
         <div className="mr-10 ml-10 xl:mr-32 xl:ml-32 2xl:max-w-[1500px] 2xl:my-0 2xl:mx-auto">
