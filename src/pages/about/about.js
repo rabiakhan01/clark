0443484c3b70859e2fcd3a8eae44db2205d6779a
rabiakhan01 <rabiakham789@gmail.com>
2024-03-23 10:44:21 +0500

    aboutDiv is deleted and the component is maped in the about file

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,8 +1,17 @@
 import React from "react";
 import images from "../../assets/images/images";
 import Button from "../../components/button/button";
-
+import aboutData from "../../utils/aboutData";
 const About = () => {
+    const data =  aboutData.map((items) => {
+        
+                   return(
+                    <div className="text-primary">
+                    <p>{items.heading}</p>
+                    <p>{items.para}</p>
+                    </div>
+                   );
+                })
     return (
         <div className="flex mt-28 h-full w-full">
             <div className="hidden md:flex relative  w-3/6 lg:-left-12 md:-left-16">
@@ -14,23 +23,8 @@ const About = () => {
                 <span className="text-base sm:text-xl  text-grayColor pb-10 md:text-start text-center">
                     A small river named Duden flows by their place and supplies it with the necessary regelialia.
                 </span>
-                <div className="flex sm:gap-x-12 gap-8 sm:text-xl text-base">
-                <div className="text-primary font-bold text-nowrap">
-                <p>Name:</p>
-                <p>Date of birth:</p>
-                <p className="p-auto">Address</p>
-                <p>Zip code</p>
-                <p>Email</p>
-                <p>Phone</p>
-                </div>
-                <div className="text-grayColor text-wrap">
-                <p>Clark Thompaso</p>
-                <p>January 01, 1987</p>
-                <p>San Francisco CA 97987 USA</p>
-                <p>1000</p>
-                <p>clarkthomp@gmail.com</p>
-                <p>+1-2234-5678-0-0</p>
-                </div>
+                <div>
+                {data}
                 </div>
                 <div className="text-2xl mb-10 mt-10">
                             <span className="text-secondary">120 </span>
---
+++ src/utils/aboutData.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        heading: 'Name',
+        para: 'Clark Thompson',
+    },
+    {
+        heading: 'Date of birth',
+        para: 'January 01, 1987',
+    },
+    {
+        heading: 'Address',
+        para: 'San Francisco CA 97987 USA',
+    },
+    {
+        heading: 'Zip code',
+        para: '1000',
+    },
+    {
+        heading: 'Email',
+        para: 'clarkthomp@gmail.com',
+    },
+    {
+        heading: 'Phone',
+        para: '+1-2234-5678-0-0',
+    },
+]
