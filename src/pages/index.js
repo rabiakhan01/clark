7d78a28a832022a7e97bd49235616ec56bf8e18b
rabiakhan01 <rabiakham789@gmail.com>
2024-03-23 18:02:53 +0500

    icons folder is added and three icons are placed in it

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../utils/layout/layout";
 import Intro from "./Intro/intro";
 import About from "./about/about";
 import Resume from "./Resume/Resume";
+import Services from "./services/services";
 const HomePage = () => {
     return (
         <>
@@ -10,6 +11,7 @@ const HomePage = () => {
         <Intro />
         <About />
         <Resume />
+        <Services />
         </Layout>
         </>
     );
