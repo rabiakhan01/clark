4e53afb231a8eafa13c148bebb9d5f0b1db9bf2d
rabiakhan01 <rabiakham789@gmail.com>
2024-03-23 19:41:11 +0500

    service card data is exported from serviceData div

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,5 +1,12 @@
 const images = {
+
     // about section
     image_about: require('./bg_1.png'),
+    
+    //icons for services section
+
+    analysis: require('./icons/analysis.png'),
+    flasks: require('./icons/flasks.png'),
+    ideas: require('./icons/think-outside-the-box.png'),
 }
 export default images;
---
+++ src/utils/serviceData.js
@@ -0,0 +1,27 @@
+import images from "../assets/images/images"
+export default [
+    {
+        icon: images.analysis,
+        content: 'web design'
+    },
+    {
+        icon: images.flasks,
+        content: 'photography'
+    },
+    {
+        icon: images.ideas,
+        content: 'web developer'
+    },
+    {
+        icon: images.analysis,
+        content: 'app developing'
+    },
+    {
+        icon: images.flasks,
+        content: 'branding'
+    },
+    {
+        icon: images.ideas,
+        content: 'project strategy'
+    },
+]
