af15261ef8bd037abab2ce921918e69b077e01b1
rabiakhan01 <rabiakham789@gmail.com>
2024-03-24 12:27:59 +0500

    layout changes happens

--- src/utils/layout/layout.js
+++ src/utils/layout/layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./navbar/navbar";
 import Footer from "./footer/footer";
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
     return (
-        <div className=" mx-5 sm:mx-10 xl:mx-32 2xl:max-w-[1440px] 2xl:my-0 2xl:mx-auto overflow-hidden">
-        <Navbar />
-        {children}
-        <Footer />
+        <div className=" mx-5 sm:mx-20 xl:mx-32 2xl:max-w-[1440px] 2xl:my-0 2xl:mx-auto overflow-hidden">
+            <Navbar />
+            {children}
+            <Footer />
         </div>
     );
 }
