91f001c64b1ca0357a6f8b2735579eca45835398
rabiakhan01 <rabiakham789@gmail.com>
2024-04-21 09:56:00 +0500

    fix

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,13 +1,5 @@
 import React from "react";
-import Intro from '../../components/home/Intro'
-import Contact from "../../components/home/Contact";
-import About from "../../components/home/About";
-import Services from "../../components/home/Services";
-import Skills from "../../components/home/Skills";
-import Blog from "../../components/home/Blog";
-import Projects from "../../components/home/Projects";
-import HireMe from "../../components/home/Hireme";
-import Resume from "../../components/home/Resume";
+import { Intro, About, Resume, Services, Skills, Blog, Projects, HireMe, Contact } from "../../components/Home";
 
 const Home = () => {
     return (
